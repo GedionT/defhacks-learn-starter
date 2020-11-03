3d4aeafbf9c10a0c4df59b47d0574efcaf08e6bc
Andrew Yang <andy.tubeee@gmail.com>
2020-11-02 22:48:07 -0500

    Footer update

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,47 +1,69 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../../styles/footer.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 import { Link } from 'react-router-dom';
 
-function Footer() {
+function Footer(props) {
   return (
-    <footer className="ftr">
+    <footer className="ftr" style={{ top: `${props.bottomMost + 150}px` }}>
       <div className="row">
-        <div className="col ml-5">
+        <div className="col">
           <div className="text-left">
             <h2>Def Hacks Inc. 2020</h2>
-            <h5 className="ml-4">Organization mission description</h5>
+            <h5>Organization mission description</h5>
           </div>
         </div>
         <div className="col-md-auto">
           <h3>Contact Us</h3>
           <ul className="ftr-col mt-4">
             <li>
-              <a href="https://www.facebook.com/defhacks" target="__blank">
+              <i
+                class="fab fa-facebook-square"
+                style={{ fontSize: '20px' }}
+              ></i>
+              <a
+                href="https://www.facebook.com/defhacks"
+                target="__blank"
+                className="ml-2"
+              >
                 <span className="h5">Facebook</span>
               </a>
             </li>
             <li>
-              <a href="https://twitter.com/def_hacks" target="__blank">
+              <i class="fab fa-twitter-square" style={{ fontSize: '20px' }}></i>
+              <a
+                href="https://twitter.com/def_hacks"
+                target="__blank"
+                className="ml-2"
+              >
                 <span className="h5">Twitter</span>
               </a>
             </li>
             <li>
-              <a href="https://www.instagram.com/def_hacks/" target="__blank">
+              <i class="fab fa-instagram" style={{ fontSize: '20px' }}></i>
+
+              <a
+                href="https://www.instagram.com/def_hacks/"
+                target="__blank"
+                className="ml-2"
+              >
                 <span className="h5">Instagram</span>
               </a>
             </li>
             <li>
+              <i class="fab fa-linkedin" style={{ fontSize: '20px' }}></i>
               <a
                 href="https://www.linkedin.com/company/def-hacks/"
                 target="__blank"
+                className="ml-2"
               >
                 <span className="h5">LinkedIn</span>
               </a>
             </li>
           </ul>
         </div>
-        <div className="col col-lg-2">
+        <div className="col col-md-auto">
           <h3>Navigate</h3>
           <ul className="ftr-col mt-4">
             <li>
@@ -50,22 +72,19 @@ function Footer() {
               </Link>
             </li>
             <li>
-              <a href="https://twitter.com/def_hacks" target="__blank">
-                <span className="h5">Twitter</span>
-              </a>
+              <Link to="/about">
+                <span className="h5">About</span>
+              </Link>
             </li>
             <li>
-              <a href="https://www.instagram.com/def_hacks/" target="__blank">
-                <span className="h5">Instagram</span>
-              </a>
+              <Link to="/explore">
+                <span className="h5">Explore</span>
+              </Link>
             </li>
             <li>
-              <a
-                href="https://www.linkedin.com/company/def-hacks/"
-                target="__blank"
-              >
-                <span className="h5">LinkedIn</span>
-              </a>
+              <Link to="/account">
+                <span className="h5">My Account</span>
+              </Link>
             </li>
           </ul>
         </div>
