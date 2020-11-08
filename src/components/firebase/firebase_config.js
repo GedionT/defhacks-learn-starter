554db462ef054f605db4fceb81326aca4ffebd45
Gedion Teshome <Teshomegedion@gmail.com>
2020-11-08 05:48:27 +0300

    dev config keys for fallback added

--- src/components/firebase/firebase_config.js
+++ src/components/firebase/firebase_config.js
@@ -1,12 +1,12 @@
 const config = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_KEY || "AIzaSyBKuxFTG8WPjJlV7G3Og84XI6oDWtnMhFs",
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN || "def-hacks-learn.firebaseapp.com" ,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE || "https://def-hacks-learn.firebaseio.com",
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "def-hacks-learn",
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "def-hacks-learn.appspot.com",
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID || 273662385914,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID || "1:273662385914:web:3b894c7384f63748853343",
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || "G-0M7KCLDBY6",
 };
 
 export default config;
