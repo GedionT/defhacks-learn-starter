96512bc4686d07a548932db7dba4c1a835bf5c9e
Gedion Teshome <Teshomegedion@gmail.com>
2020-11-08 06:05:11 +0300

    Update firebase_config.js

--- src/components/firebase/firebase_config.js
+++ src/components/firebase/firebase_config.js
@@ -1,12 +1,12 @@
 const config = {
-  apiKey: "AIzaSyBKuxFTG8WPjJlV7G3Og84XI6oDWtnMhFs",
-  authDomain: "def-hacks-learn.firebaseapp.com" ,
-  databaseURL: "https://def-hacks-learn.firebaseio.com",
-  projectId: "def-hacks-learn",
-  storageBucket: "def-hacks-learn.appspot.com",
+  apiKey: `AIzaSyBKuxFTG8WPjJlV7G3Og84XI6oDWtnMhFs`,
+  authDomain: `def-hacks-learn.firebaseapp.com`,
+  databaseURL: `https://def-hacks-learn.firebaseio.com`,
+  projectId: `def-hacks-learn`,
+  storageBucket: `def-hacks-learn.appspot.com`,
   messagingSenderId: 273662385914,
-  appId: "1:273662385914:web:3b894c7384f63748853343",
-  measurementId: "G-0M7KCLDBY6",
+  appId: `1:273662385914:web:3b894c7384f63748853343`,
+  measurementId: `G-0M7KCLDBY6`,
 };
 
 export default config;
