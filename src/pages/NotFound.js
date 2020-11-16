3f8cc233a205cc1ab12125f0ae502a81028b464f
Gedion Teshome <Teshomegedion@gmail.com>
2020-11-16 06:15:41 +0300

    Update NotFound.js

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles/NotFound.css';
-import { Route, Switch, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
