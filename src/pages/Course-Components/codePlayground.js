607ae6eb448f237dfeee63ae2592d362629dd9b7
Gedion Teshome <Teshomegedion@gmail.com>
2020-11-16 06:11:50 +0300

    Update codePlayground.js

--- src/pages/Course-Components/codePlayground.js
+++ src/pages/Course-Components/codePlayground.js
@@ -1,17 +1,17 @@
-import React, { useState, useRef } from 'react';
+import React, { useRef } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Row, Col } from 'react-bootstrap';
+// import { Row, Col } from 'react-bootstrap';
 import '@fortawesome/fontawesome-free/css/all.css';
 import '../../styles/VideoPlayer.css';
 import CodeEditor from './codeEditor';
-import OutputPanel from './outputPanel';
+// import OutputPanel from './outputPanel';
 
 function CodePlayground() {
   const editor = useRef(null);
 
-  const focusEditor = () => {
-    editor.current.focus();
-  };
+//   const focusEditor = () => {
+//     editor.current.focus();
+//   };
 
   return (
     <div style={{ marginRight: '500px' }}>
