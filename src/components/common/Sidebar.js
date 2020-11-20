df6f05f27b04d1594c338650b1a43bff339255a8
Anh Le <kunquan2345@gmail.com>
2020-11-20 02:13:08 -0800

    UPDATED: add universal Sidebar to Dashboard, ExistActivity, ExistUser
    _ Use the same Sidebar.js in all those components for later styling
    _ Add z-index on the "decoration element" such that it won't block other elements

--- src/components/common/Sidebar.js
+++ src/components/common/Sidebar.js
@@ -5,40 +5,41 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Sidebar() {
   return (
-    <div className="exist-main">
-      <div className="exist-menu">
-        <div className="exist-pattern"></div>
-        <div className="shape-circle"></div>
-        <div className="shape-tri"></div>
-        <div className="sub-menu-1">
-          <Link to="/account" className="color">
-            Account
-          </Link>
-        </div>
-        <br />
-        <div className="sub-menu-2">
-          <Link to="/ExistUser" className="color">
-            Courses
-          </Link>
-        </div>
-        <br />
-        <div className="sub-menu-3">
-          <Link to="/ExistActivity" className="color">
-            Activity
-          </Link>
-        </div>
-        <br />
-        <div className="sub-menu-4">
-          <Link className="color" onClick={() => alert('Coming soon!')}>
-            Settings
-          </Link>
-        </div>
-        <br />
-        <div className="sub-menu-5">
-          <Link to="/About" className="color">
-            About
-          </Link>
-        </div>
+    <div className="exist-menu">
+      {/* This part go to background as decoration */}
+      <div className="exist-pattern"></div>
+      <div className="shape-circle"></div>
+      <div className="shape-tri"></div>
+
+      {/* Actually button */}
+      <div className="sub-menu">
+        <Link to="/account" className="color">
+          Account
+        </Link>
+      </div>
+      <br />
+      <div className="sub-menu">
+        <Link to="/ExistUser" className="color">
+          Courses
+        </Link>
+      </div>
+      <br />
+      <div className="sub-menu">
+        <Link to="/ExistActivity" className="color">
+          Activity
+        </Link>
+      </div>
+      <br />
+      <div className="sub-menu">
+        <Link className="color" onClick={() => alert('Coming soon!')}>
+          Settings
+        </Link>
+      </div>
+      <br />
+      <div className="sub-menu">
+        <Link to="/About" className="color">
+          About
+        </Link>
       </div>
     </div>
   );
--- src/pages/ExistActivity.js
+++ src/pages/ExistActivity.js
@@ -1,55 +1,32 @@
 import React from 'react';
-import '../styles/exist.css';
-import { Link } from 'react-router-dom';
+
+import { Row, Col } from 'react-bootstrap';
 import Calendar from '../components/calendar/calendar';
+
+import Sidebar from '../components/common/Sidebar';
+import '../styles/exist.css';
 
 function ExistActivity() {
   return (
-    <div className="exist-main">
-      <div className="exist-menu">
-        <div className="exist-pattern"></div>
-        <div className="shape-circle"></div>
-        <div className="shape-tri"></div>
-        <div className="sub-menu-1">
-          <Link to="/account" className="color">
-            Account
-          </Link>
-        </div>
-        <br />
-        <div className="sub-menu-2">
-          <Link to="/ExistUser" className="color">
-            Courses
-          </Link>
-        </div>
-        <br />
-        <div className="sub-menu-3">
-          <Link to="/ExistActivity" className="color">
-            Activity
-          </Link>
-        </div>
-        <br />
-        <div className="sub-menu-4">
-          <Link className="color" onClick={() => alert('Coming soon!')}>
-            Settings
-          </Link>
-        </div>
-        <br />
-        <div className="sub-menu-5">
-          <Link to="/About" className="color">
-            About
-          </Link>
-        </div>
-      </div>
-      <div className="blue-box-1"></div>
-      <div className="blue-box-2"></div>
-      <div className="exist-title">$username</div>
-      <br />
-      <div className="green-box-1"></div>
-      <div className="green-box-2"></div>
-      <div className="act-cont">Daily Activity</div>
-      <div className="cal-act">
-        <Calendar className="cal-act-1" />
-      </div>
+    <div className="general_container">
+      <Row>
+        <Col xs={3} lg={3}>
+          <Sidebar />
+        </Col>
+
+        <Col xs={9} lg={9}>
+          <div className="blue-box-1"></div>
+          <div className="blue-box-2"></div>
+          <div className="exist-title">$username</div>
+          <br />
+          <div className="green-box-1"></div>
+          <div className="green-box-2"></div>
+          <div className="act-cont">Daily Activity</div>
+          <div className="cal-act">
+            <Calendar className="cal-act-1" />
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 }
