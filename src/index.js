51ffb201d2f4eb8bb572fdf6900a64860adf9f46
gediont <teshomegedion@gmail.com>
2020-11-29 03:42:36 +0300

    wrapped entire app with AuthProvider context API

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 
+import { AuthProvider } from './components/firebase/context';
 import App from './App';
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <AuthProvider>
+      <Router>
+        <App />
+      </Router>
+    </AuthProvider>
   </React.StrictMode>,
   rootElement
 );
