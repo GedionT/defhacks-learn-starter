402078cb9d5ab395099f48b3d42ba6160034b21d
Andrew Yang <andy.tubeee@gmail.com>
2020-12-21 18:54:59 -0500

    Get courses from firestore finished including frontend

--- src/pages/ExistUser.js
+++ src/pages/ExistUser.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import { useHistory } from 'react-router-dom';
 
@@ -12,55 +12,59 @@ function ExistUser() {
   const { user } = useContext(AppContext);
   const history = useHistory();
 
+  const [courseList, setCourseList] = useState([]);
+
+  const [lessonList, setLessonList] = useState([]);
+
+  function getDocument() {
+    const coursesRef = db.collection('Courses');
+    var tempDoc = Array();
+    coursesRef.get().then((querySnapshot) => {
+      querySnapshot.forEach((docName) => {
+        setCourseList((oldCl) => [...oldCl, { name: docName.id }]);
+        querySnapshot.forEach((field) => {
+          console.log(field.data());
+          field.data().courseJSON.forEach((lesson) => {
+            console.log(lesson.lesson_name);
+            setLessonList((previousLesson) => [
+              ...previousLesson,
+              lesson.lesson_name,
+            ]);
+          });
+        });
+      });
+    });
+  }
+
+  function getLessonDocuments(collection) {
+    courseList.forEach((course) => {
+      console.log(course);
+    });
+  }
+
   useEffect(() => {
     if (!user) {
       history.push('/signin');
     }
   });
-  var coursesList = [];
-  async function getDocuments() {
-    const events = await db.collection('Courses');
-    events.get().then((querySnapshot) => {
-      const tempDoc = querySnapshot.docs.map((doc) => {
-        return { id: doc.id };
-      });
-      tempDoc.forEach((doc) => {
-        console.log(doc.id);
-      });
-    });
-  }
 
   useEffect(() => {
-    const coursesRef = db.collection('Courses');
-    coursesRef.get().then((querySnapshot) => {
-      const tempDoc = querySnapshot.docs.map((doc) => {
-        return { id: doc.id };
-      });
-      tempDoc.forEach((docID) => {
-        console.log(docID.id);
-        try {
-          coursesList.push(docID.id);
-          console.log('Pushed');
-        } catch (e) {
-          console.log('Push failed, ' + e);
-        }
-      });
-    });
-    db.collection('Courses')
-      .doc('Introduction to Web Development')
-      .collection('HTML')
-      .get()
-      .then((snapshot) => {
-        snapshot.forEach((doc) => {
-          if (!doc.exists) {
-            console.log("Doesn't exist");
-          } else {
-            console.log('Exist');
-          }
-        });
-      });
+    getDocument();
+    console.log(courseList);
   }, []);
 
+  const coursesListComponent = courseList.map((c) => {
+    return (
+      <li>
+        {c.name}
+        <ul>
+          {lessonList.map((lesson) => {
+            return <li>{lesson}</li>;
+          })}
+        </ul>
+      </li>
+    );
+  });
   return (
     <div className="general_container">
       <Row style={{ marginRight: 0, marginLeft: 0 }}>
@@ -71,7 +75,6 @@ function ExistUser() {
         <Col xs={9} lg={9} style={{ position: 'relative' }}>
           <div className="blue-box-1"></div>
           <div className="blue-box-2">
-            {' '}
             <div className="exist-title">Welcome back, {user.displayName}!</div>
           </div>
 
@@ -79,11 +82,7 @@ function ExistUser() {
           <div className="content-box-2 pl-2">
             <div className="box-title">My Courses</div>
             <div className="box-content">
-              <ul>
-                {coursesList.map((course) => {
-                  return <li>{course}</li>;
-                })}
-              </ul>
+              <ul>{coursesListComponent}</ul>
             </div>
           </div>
         </Col>
